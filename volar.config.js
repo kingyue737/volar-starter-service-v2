777c702960dfd7f128f00ebad581e1c29c5f507f
Yue JIN <yjin@nustarnuclear.com>
2024-03-07 15:34:47 +0800

    feat: config prettyhtml plugin

--- volar.config.js
+++ volar.config.js
@@ -1,5 +1,12 @@
+console.log("print something");
+
 module.exports = {
-    services: [
-        require('volar-service-prettyhtml').default({ printWidth: 10 }),
-    ],
+  // services: [require("volar-service-prettyhtml").create({ printWidth: 10 })],
+  services: [
+    require("volar-service-prettyhtml").create({
+      getFormattingOptions() {
+        return { printWidth: 10 };
+      },
+    }),
+  ],
 };
